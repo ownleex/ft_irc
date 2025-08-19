c64c57ce3e6bb61c2ee2d92254bb51e758feae4f
Isolation <parodie.clement@gmail.com>
2025-08-19 06:08:56 +0200

    serv_run avec l'init du socket et de poll

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
 #include <string>
+#include <map>
+#include <vector>
 #include <iostream>
+#include <stdexcept>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
 #include "Client.hpp"
 
 class Server
@@ -9,6 +17,14 @@ class Server
     private:
         int _port;
         std::string _password;
+        int _serverSocket;
+        std::vector<pollfd> _pollfds;
+        std::map<int, Client> _clients;
+
+        void initSocket();
+        void newConnection();
+        void clientData();
+        void removeClient();
 
     public:
         Server(int port, const std::string &password);
